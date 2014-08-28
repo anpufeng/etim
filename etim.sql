c4c0fbb97bfac93f21fe89322b9b5009585dad3a
Ethan <anpufeng@gmail.com>
2014-08-28 09:16:09 +0800

    修改数据SQL文件

--- etim.sql
+++ etim.sql
@@ -11,7 +11,7 @@
  Target Server Version : 50619
  File Encoding         : utf-8
 
- Date: 08/26/2014 20:07:52 PM
+ Date: 08/28/2014 09:02:31 AM
 */
 
 SET NAMES utf8;
@@ -29,6 +29,13 @@ CREATE TABLE `event` (
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
+--  Records of `event`
+-- ----------------------------
+BEGIN;
+INSERT INTO `event` VALUES ('1', '1', b'1');
+COMMIT;
+
+-- ----------------------------
 --  Table structure for `friend`
 -- ----------------------------
 DROP TABLE IF EXISTS `friend`;
@@ -41,6 +48,13 @@ CREATE TABLE `friend` (
   `req_status` tinyint(4) NOT NULL DEFAULT '0' COMMENT '0:请求未发送 1:请求已发送 2：请求被拒绝 3：请求已同意',
   PRIMARY KEY (`friend_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+--  Records of `friend`
+-- ----------------------------
+BEGIN;
+INSERT INTO `friend` VALUES ('1', '1', '2', '2014-08-26 14:33:53', '2014-08-26 14:33:53', '0'), ('2', '1', '2', '2014-08-26 14:36:05', '2014-08-26 14:36:05', '0'), ('3', '19', '20', '2014-08-26 19:07:57', '2014-08-26 19:07:57', '0'), ('4', '17', '20', '2014-08-26 19:09:03', '2014-08-26 19:09:03', '0'), ('5', '17', '21', '2014-08-26 19:12:43', '2014-08-26 19:12:43', '0'), ('6', '17', '21', '2014-08-26 19:12:47', '2014-08-26 19:12:47', '0'), ('7', '17', '21', '2014-08-26 19:12:49', '2014-08-26 19:12:49', '0');
+COMMIT;
 
 -- ----------------------------
 --  Table structure for `message`
@@ -67,6 +81,13 @@ CREATE TABLE `status` (
 ) ENGINE=InnoDB AUTO_INCREMENT=40000 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
+--  Records of `status`
+-- ----------------------------
+BEGIN;
+INSERT INTO `status` VALUES ('1', '在线'), ('2', '隐身'), ('3', '离开'), ('4', '离线');
+COMMIT;
+
+-- ----------------------------
 --  Table structure for `user`
 -- ----------------------------
 DROP TABLE IF EXISTS `user`;
@@ -82,4 +103,11 @@ CREATE TABLE `user` (
   PRIMARY KEY (`user_id`),
   UNIQUE KEY `username_index` (`username`)
 ) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+--  Records of `user`
+-- ----------------------------
+BEGIN;
+INSERT INTO `user` VALUES ('17', 'admin', 'admin', '2014-08-26 17:17:52', '2014-08-26 17:17:52', '暂无签名', '0', '1'), ('18', 'admin2', 'Admin', '2014-08-26 17:19:39', '2014-08-26 17:19:39', '暂无签名', '0', '1');
+COMMIT;
 
